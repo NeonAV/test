50eac8f4e1c937c4b3c5aabc178a90ad8043fc5f
Albert Stepantsev <philfromfmf@mail.ru>
2015-03-02 21:29:58 +0300

    PDO

--- www/classes/DB.php
+++ www/classes/DB.php
@@ -2,27 +2,17 @@
 
 class DB
 {
+    private $dbh;
+
     public function __construct()
     {
-        mysql_connect('localhost', 'root', '');
-        mysql_select_db('test');
+        $this->dbh = new PDO('mysql:dbname=test;host=localhost', 'root', '');
     }
 
-    public function queryAll($sql, $class = 'stdClass')
+    public function query($sql, $params=[])
     {
-        $res = mysql_query($sql);
-        if (false === $res) {
-            return false;
-        }
-        $ret = [];
-        while ($row = mysql_fetch_object($res, $class)) {
-            $ret[] = $row;
-        }
-        return $ret;
-    }
-
-    public function queryOne($sql, $class = 'stdClass')
-    {
-        return $this->queryAll($sql, $class)[0];
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute($params);
+        return $sth->fetchAll(PDO::FETCH_OBJ);
     }
 }
--- www/controllers/NewsController.php
+++ www/controllers/NewsController.php
@@ -5,15 +5,28 @@ class NewsController
 
     public function actionAll()
     {
-        $items = News::getAll();
-        include __DIR__ . '/../views/news/all.php';
+        $db = new DB();
+        $res = $db->query(
+            'SELECT * FROM news WHERE id=:id',
+            [':id' => 2]
+        );
+        var_dump($res);
+        die;
+        /*
+        $news = News::getAll();
+        $view = new View();
+        $view->items = $news;
+        $view->display('news/all.php');
+        */
     }
 
     public function actionOne()
     {
         $id = $_GET['id'];
         $item = News::getOne($id);
-        include __DIR__ . '/../views/news/one.php';
+        $view = new View();
+        $view->assign('item', $item);
+        $view->display('news/one.php');
     }
 
 }
